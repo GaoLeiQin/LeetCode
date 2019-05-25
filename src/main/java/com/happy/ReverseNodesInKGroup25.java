59d1bb255dab2bd97c457fe452286e0a966985c2
GaoLeiQin <1031819389@qq.com>
2019-05-25 17:56:34 +0800

    第25题
    k个一组翻转链表

---
+++ src/main/java/com/happy/ReverseNodesInKGroup25.java
@@ -0,0 +1,107 @@
+package com.happy;
+
+import com.happy.Common.ListNode;
+
+import java.util.Stack;
+
+/**
+ * 第25题 k个一组翻转链表
+ * 给出一个链表，每 k个节点一组进行翻转，并返回翻转后的链表。
+ *
+ * @author qgl
+ * @date 2019/05/25
+ */
+public class ReverseNodesInKGroup25 {
+    /**
+     * 解法一：借用队列实现
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        while (true) {
+            int count = 0;
+            ListNode tmp = head;
+            while (tmp != null && count < k) {
+                stack.push(tmp);
+                tmp = tmp.next;
+                count++;
+            }
+            if (count != k) {
+                p.next = head;
+                break;
+            }
+            while (!stack.isEmpty()) {
+                p.next = stack.pop();
+                p = p.next;
+            }
+            p.next = tmp;
+            head = tmp;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 解法二：尾插法
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode tail = dummy;
+        while (true) {
+            int count = 0;
+            while (tail != null && count != k) {
+                count++;
+                tail = tail.next;
+            }
+            if (tail == null) break;
+            ListNode head1 = pre.next;
+            while (pre.next != tail) {
+                ListNode cur = pre.next;
+                pre.next = cur.next;
+                cur.next = tail.next;
+                tail.next = cur;
+            }
+            pre = head1;
+            tail = head1;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 解法三：递归
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup3(ListNode head, int k) {
+        ListNode cur = head;
+        int count = 0;
+        while (cur != null && count != k) {
+            cur = cur.next;
+            count++;
+        }
+        if (count == k) {
+            cur = reverseKGroup3(cur, k);
+            while (count != 0) {
+                count--;
+                ListNode tmp = head.next;
+                head.next = cur;
+                cur = head;
+                head = tmp;
+            }
+            head = cur;
+        }
+        return head;
+    }
+}
---
+++ src/test/java/com/happy/Test25.java
@@ -0,0 +1,29 @@
+package com.happy;
+
+import com.happy.Common.ListNode;
+import org.junit.Test;
+
+/**
+ * 第25题 k个一组翻转链表
+ *
+ * @author qgl
+ * @date 2019/05/25
+ */
+public class Test25 {
+    @Test
+    public void test25() {
+        ReverseNodesInKGroup25 reverseNodesInKGroup25 = new ReverseNodesInKGroup25();
+        ListNode head = new ListNode(1);
+        ListNode two = new ListNode(5);
+        ListNode three = new ListNode(4);
+        ListNode four = new ListNode(8);
+        head.next = two;
+        two.next = three;
+        three.next = four;
+
+        int k = 3;
+        ListNode swapList = reverseNodesInKGroup25.reverseKGroup(head, k);
+        System.out.println("k个一组翻转链表后的结果: " + Common.printListNode(swapList));
+    }
+
+}
