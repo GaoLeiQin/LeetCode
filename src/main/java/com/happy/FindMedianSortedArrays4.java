5e72c75edc67d5f3f509ac42315857de8d1d62da
GaoLeiQin <1031819389@qq.com>
2019-07-22 21:43:43 +0800

    第4题
    优化解法

--- src/main/java/com/happy/FindMedianSortedArrays4.java
+++ src/main/java/com/happy/FindMedianSortedArrays4.java
@@ -13,12 +13,7 @@ public class FindMedianSortedArrays4 {
         int m = nums1.length;
         int n = nums2.length;
         if (m > n) {
-            int[] temp = nums1;
-            nums1 = nums2;
-            nums2 = temp;
-            int tmp = m;
-            m = n;
-            n = tmp;
+            return findMedianSortedArrays(nums2, nums1);
         }
         int iMin = 0, iMax = m, halfLength = (m + n + 1) / 2;
         while (iMin <= iMax) {
@@ -29,6 +24,7 @@ public class FindMedianSortedArrays4 {
             } else if (i > iMin && nums1[i - 1] > nums2[j]) {
                 iMax = i - 1;
             } else {
+                // 达到要求，将边界条件列出来单独考虑
                 int maxLeft = 0;
                 if (i == 0) {
                     maxLeft = nums2[j - 1];
@@ -37,6 +33,7 @@ public class FindMedianSortedArrays4 {
                 } else {
                     maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
                 }
+                // 奇数不需要考虑右半部分
                 if ((m + n) % 2 == 1) {
                     return maxLeft;
                 }
